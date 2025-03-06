40ac54d65b24173d3a59f9c28fd930cb7e0fee1f
VermountW <vermountwilliam@gmail.com>
2025-03-06 19:24:37 +0800

    margin

--- app/Shopping/page.tsx
+++ app/Shopping/page.tsx
@@ -17,19 +17,18 @@ const ShoppingPage = () => {
         <div className="absolute inset-0 bg-black/15"></div>
         <div className="relative h-full flex items-center justify-center text-center">
           <div className="text-white px-4">
-            <p className="text-6xl font-bold tracking-wider">What to Eat</p>
+            <p className="text-6xl font-bold tracking-wider">Where to Shop ?</p>
           </div>
         </div>
       </div>
-
+      {/* Main Content and Sidebar Layout */}
       <section className="py-16 px-4 md:px-0">
         <div className="container mx-auto max-w-7xl">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             {/* Main Content */}
             <div className="md:col-span-3 space-y-16">
               <article className="space-y-16">
-                <div className="space-y-6 text-black text-xl text-justify leading-relaxed">
-                  <h2 className="text-3xl font-bold mb-6 text-gray-800">Do you Want Shopping?</h2>
+                <div className="text-black text-xl text-justify leading-relaxed">
                   <p>
                     Shopping centers are spread all over Jakarta. You could find and buy anything in Jakarta, from the cheapest to the most expensive. Here are some recommended shopping centers you could visit :
                   </p>
@@ -46,10 +45,15 @@ const ShoppingPage = () => {
                       className="object-cover rounded-lg"
                     />
                   </div>
-                  <p className="text-black text-xl text-justify leading-relaxed">
-                    Grand Indonesia is the biggest mall in Jakarta. It offers a premium international shopping experience, a wide range of fashion brands, restaurants, and entertainment options. If you're interested in luxury brands, this is the place to visit.
-                  </p>
-                  <div className="w-full h-[400px] mt-6">
+                  <div className="space-y-4 text-black text-xl text-justify leading-relaxed">
+                    <p>
+                    Grand Indonesia is the largest and most prestigious shopping mall in Jakarta, offering a luxurious retail and lifestyle experience. Located in the heart of the city, it features high-end international brands, premium local boutiques, and a modern design. The mall is divided into East and West sections, connected by the Skybridge, making it easy to explore its diverse shopping options.
+                    </p>
+                    <p>
+                    Beyond shopping, Grand Indonesia offers world-class dining and entertainment. Visitors can enjoy fine dining, trendy cafés, a state-of-the-art cinema, and a mesmerizing musical fountain show. Whether for luxury shopping or leisure, it is a top destination for both locals and tourists in Jakarta.
+                    </p>
+                  </div>
+                  <div className="w-[600px] h-[400px] mt-6">
                     <iframe 
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.517963712404!2d106.81825251468562!3d-6.195180095515055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f421c2ebd463%3A0xccfcc89b95aaf1ae!2sGrand%20Indonesia!5e0!3m2!1sid!2stw!4v1672065621566!5m2!1sid!2stw" 
                       className="w-full h-full border-0 rounded-lg" 
@@ -70,10 +74,15 @@ const ShoppingPage = () => {
                       className="object-cover rounded-lg"
                     />
                   </div>
-                  <p className="text-black text-xl text-justify leading-relaxed">
-                    Mangga Dua is a shopping center offering various goods, including textiles, souvenirs, and accessories like clothing for men, women, and children. It also has international products from China, Korea, Hong Kong, and Thailand at affordable prices.
-                  </p>
-                  <div className="w-full h-[400px] mt-6">
+                  <div className="space-y-4 text-black text-xl text-justify leading-relaxed">
+                    <p>
+                      Mangga Dua is a well-known shopping center in Jakarta, offering a wide range of products at affordable prices. It is a popular destination for those looking for textiles, souvenirs, and accessories, as well as clothing for men, women, and children. With its diverse selection of items, the shopping center attracts both local and international visitors searching for great deals.
+                    </p>
+                    <p>
+                      In addition to local goods, Mangga Dua also features international products from China, Korea, Hong Kong, and Thailand. Shoppers can find everything from fashion and electronics to household items, making it a one-stop destination for bargain hunters. Its competitive prices and vast selection make it a favorite shopping hub in Jakarta.
+                    </p>
+                  </div>
+                  <div className="w-[600px] h-[400px] mt-6">
                     <iframe 
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.952911642041!2d106.82213601468527!3d-6.1370290955565165!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5f8cafe5231%3A0x9c53d9645074bdfe!2sMangga%20Dua%20Mall!5e0!3m2!1sid!2stw!4v1672065738557!5m2!1sid!2stw" 
                       className="w-full h-full border-0 rounded-lg" 
@@ -94,16 +103,22 @@ const ShoppingPage = () => {
                       className="object-cover rounded-lg"
                     />
                   </div>
-                  <p className="text-black text-xl text-justify leading-relaxed">
-                    Pasar Baru is the oldest shopping center in Jakarta, located on Jalan Pasar Baru, Central Jakarta. It features clothing stores, tailor shops, shoe stores, eyewear shops, and gold jewelry shops. It's a popular thrifting destination for millennials.
-                  </p>
-                  <div className="w-full h-[400px] mt-6">
+                  <div className="space-y-4 text-black text-xl text-justify leading-relaxed">
+                    <p>
+                      Pasar Baru is the oldest shopping center in Jakarta, located on Jalan Pasar Baru, Central Jakarta. It features clothing stores, tailor shops, shoe stores, eyewear shops, and gold jewelry shops. It's a popular thrifting destination for millennials.
+                    </p>
+                    <p>
+                    In recent years, Pasar Baru has become a popular thrifting spot for millennials looking for stylish and affordable fashion. Many vintage and second-hand clothing stores offer unique finds at budget-friendly prices. Its nostalgic atmosphere, diverse selection, and bargain-friendly deals make it a must-visit shopping hub in Jakarta.
+                    </p>
+                  <div/>
+                  <div className="w-[600px] h-[400px] mt-6">
                     <iframe 
                       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15866.936752778181!2d106.82515082561707!3d-6.166339928569542!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5cf671b73c7%3A0x7c42154ce46ec4ef!2sPs.%20Baru%2C%20Kecamatan%20Sawah%20Besar%2C%20Kota%20Jakarta%20Pusat%2C%20Daerah%20Khusus%20Ibukota%20Jakarta%2C%20Indonesia!5e0!3m2!1sid!2stw!4v1672065667879!5m2!1sid!2stw" 
                       className="w-full h-full border-0 rounded-lg" 
                       allowFullScreen 
                       loading="lazy"
                     ></iframe>
+                  </div>
                   </div>
                 </div>
               </article>
--- app/whattoeat/page.tsx
+++ app/whattoeat/page.tsx
@@ -27,35 +27,23 @@ const TraditionalFood = () => {
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             {/* Main Content */}
             <div className="md:col-span-3 space-y-16">
-              <article className="space-y-8">
-                <div className="relative w-full h-[500px]">
-                  <Image 
-                    src="/assets/img/traditionalfood.jpg" 
-                    alt="Traditional Food"
-                    fill
-                    className="object-cover"
-                  />
-                </div>
-                
-                <div className="space-y-4 text-black text-xl text-justify leading-relaxed">
+              <article className="space-y-16">
                   <h2 className="text-3xl font-bold mb-6 text-gray-800">Traditional Food</h2>
+                  <div className="text-black text-xl text-justify leading-relaxed">
                   <p>
-                  As a capital city of Indonesia, Jakarta provides a lot of varieties of traditional food all around Indonesia. Our traditional food is made from the best herbs and spices that will give you the incredible taste.
-                  </p>
-                  <p>
-                    These are some food recommendation that you really need to taste.
+                  As a capital city of Indonesia, Jakarta provides a lot of varieties of traditional food all around Indonesia. Our traditional food is made from the best herbs and spices that will give you the incredible taste. These are some food recommendation that you really need to taste :
                   </p>
                 </div>
                 
                 {/* Food Item 1 */}
-                <div className="space-y-4">
+                <div className="space-y-6">
                   <h2 className="text-3xl font-bold mb-6 text-gray-800">1. Soto</h2>
                   <div className="relative w-full h-[500px]">
                     <Image 
                       src="/assets/img/soto.jpg" 
                       alt="Soto"
                       fill
-                      className="object-cover"
+                      className="object-cover rounded-lg"
                     />
                   </div>
                   <p className="text-black text-xl text-justify leading-relaxed">
@@ -64,30 +52,30 @@ const TraditionalFood = () => {
                 </div>
                 
                 {/* Food Item 2 */}
-                <div className="space-y-4">
+                <div className="space-y-6">
                   <h2 className="text-3xl font-bold mb-6 text-gray-800">2. Ketoprak</h2>
                   <div className="relative w-full h-[500px]">
                     <Image 
-                      src="/assets/img/ketoprak.jpg" 
-                      alt="Ketoprak"
+                      src="/assets/img/traditionalfood.jpg" 
+                      alt="nasi uduk"
                       fill
-                      className="object-cover"
+                      className="object-cover rounded-lg"
                     />
                   </div>
                   <p className="text-black text-xl text-justify leading-relaxed">
-                    Ketoprak consists of tofu, vermicelli, cucumber, bean sprouts, and you can also use boiled eggs topped with peanut sauce, sweet soy sauce, and a sprinkling of fried shallots. It can also be served with additional crackers or chips. Some versions also include tempeh.
+                  Nasi Uduk is a flavorful Indonesian dish made with fragrant coconut milk-infused rice, typically served with a variety of accompaniments. It includes crispy fried chicken, boiled eggs, cucumber slices, tempeh, and a generous portion of sambal for a spicy kick. The dish is often topped with fried shallots for added crunch and flavor. To complete the experience, it's commonly served with crispy crackers or chips on the side. Nasi Uduk offers a perfect blend of savory, spicy, and aromatic flavors that will leave you craving more.
                   </p>
                 </div>
                 
                 {/* Food Item 3 */}
-                <div className="space-y-4">
+                <div className="space-y-6">
                   <h2 className="text-3xl font-bold mb-6 text-gray-800">3. Satay</h2>
                   <div className="relative w-full h-[500px]">
                     <Image 
                       src="/assets/img/satay.jpg" 
                       alt="Satay"
                       fill
-                      className="object-cover"
+                      className="object-cover rounded-lg"
                     />
                   </div>
                   <p className="text-black text-xl text-justify leading-relaxed">
@@ -96,75 +84,75 @@ const TraditionalFood = () => {
                 </div>
                 
                 {/* Restaurant Recommendations */}
-                <div className="space-y-6">
-                  <h2 className="text-3xl font-bold mb-6 text-gray-800">Restaurant Recommendation</h2>
+                  <h2 className="text-3xl font-bold mb-6 text-gray-800">Restaurant Recomendation</h2>
+                  <div className="text-black text-xl text-justify leading-relaxed">
+                    <p>
+                      Now that you know a bit about Indonesia's traditional food, here are some recommended restaurants in Jakarta where you can savor these incredible dishes. Whether you're craving a classic sate or a hearty rendang, these restaurants will bring the best of Indonesia's diverse food culture right to your plate :
+                    </p>
+                  </div>
                   
                   {/* Restaurant 1 */}
-                  <div className="space-y-4">
-                    <h2 className="text-3xl font-bold mb-6 text-gray-800">1. Lembur Kuring</h2>
+                  <div className="space-y-6">
+                    <h2 className="text-3xl font-bold text-gray-800">1. Lembur Kuring</h2>
                     <div className="relative w-full h-[500px]">
                       <Image 
                         src="/assets/img/kuring.jpg" 
                         alt="Lembur Kuring"
                         fill
-                        className="object-cover"
+                        className="object-cover rounded-lg"
                       />
                     </div>
-                    <div className="w-full h-[400px]">
+                    <div className="w-[600px] h-[400px]">
                       <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10141.930919157612!2d106.6865601305234!3d-6.113948762943761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a02d9963fb003%3A0x1d231ede33d44861!2sRumah%20Makan%20Lembur%20Kuring!5e0!3m2!1sid!2stw!4v1672075528006!5m2!1sid!2stw" 
-                        className="w-full h-full border-0" 
+                        className="w-full h-full border-0 rounded" 
                         allowFullScreen 
                         loading="lazy" 
-                        referrerPolicy="no-referrer-when-downgrade"
                       ></iframe>
                     </div>
                   </div>
                   
                   {/* Restaurant 2 */}
-                  <div className="space-y-4">
-                    <h2 className="text-3xl font-bold mb-6 text-gray-800">2. Bandar Djakarta</h2>
+                  <div className="space-y-6">
+                    <h2 className="text-3xl font-bold text-gray-800">2. Bandar Djakarta</h2>
                     <div className="relative w-full h-[500px]">
                       <Image 
                         src="/assets/img/bandar.jpg" 
                         alt="Bandar Djakarta"
                         fill
-                        className="object-cover"
+                        className="object-cover rounded-lg"
                       />
                     </div>
-                    <div className="w-full h-[400px]">
+                    <div className="w-[600px] h-[400px]">
                       <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d253818.94733328655!2d106.58163998056088!3d-6.274111139189813!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6a1e3d05306511%3A0x820b6f7432af7bcd!2sBandar%20Djakarta!5e0!3m2!1sid!2stw!4v1672075603392!5m2!1sid!2stw" 
-                        className="w-full h-full border-0" 
+                        className="w-full h-full border-0 rounded" 
                         allowFullScreen 
                         loading="lazy" 
-                        referrerPolicy="no-referrer-when-downgrade"
                       ></iframe>
                     </div>
                   </div>
                   
                   {/* Restaurant 3 */}
-                  <div className="space-y-4">
-                    <h2 className="text-3xl font-bold mb-6 text-gray-800">3. Sate Khas Senayan</h2>
+                  <div className="space-y-6">
+                    <h2 className="text-3xl font-bold text-gray-800">3. Sate Khas Senayan</h2>
                     <div className="relative w-full h-[500px]">
                       <Image 
                         src="/assets/img/senayan.jpg" 
                         alt="Sate Khas Senayan"
                         fill
-                        className="object-cover"
+                        className="object-cover rounded-lg"
                       />
                     </div>
-                    <div className="w-full h-[400px]">
+                    <div className="w-[600px] h-[400px]">
                       <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126920.7740075372!2d106.74266125139047!3d-6.227535550415616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f15f15800229%3A0x193158d993c78a6e!2sSate%20Khas%20Senayan!5e0!3m2!1sid!2stw!4v1672075940496!5m2!1sid!2stw" 
-                        className="w-full h-full border-0" 
+                        className="w-full h-full border-0 rounded" 
                         allowFullScreen 
                         loading="lazy" 
-                        referrerPolicy="no-referrer-when-downgrade"
                       ></iframe>
                     </div>
                   </div>
-                </div>
               </article>
             </div>
 
